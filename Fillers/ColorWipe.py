b4ddb891bab41e29a4ad74f6e5d1fcd1e9d2f41d
dizzi <nicofirst1@gmail.com>
2019-11-22 16:37:34 +0100

    add color wipe and strobe

---
+++ Fillers/ColorWipe.py
@@ -0,0 +1,47 @@
+
+from Fillers.Default import Default
+from RGB import RGB
+
+
+class ColorWipe(Default):
+    data_type = "ColorWipe"
+
+    def __init__(self, rate, color='rand', alpha=255):
+        """
+        Init for steady color
+        :param args: for App
+        :param trail_length: length of snow trail
+        """
+        super().__init__(rate)
+
+        if color=="rand":
+            color=RGB(random=True)
+
+        self.color = color
+        self.alpha=alpha
+        self.step=1
+        self.reverse=False
+
+
+    def fill(self):
+
+        self.color.update_single(c=self.alpha)
+
+        step=self.step
+        color=self.color
+
+        if self.reverse:
+            color=RGB()
+
+        for idx in range(self.strip_length):
+            if idx< step:
+                self.color_set(idx,color)
+
+
+        self.step+=1
+
+        if self.step>self.strip_length :
+            self.reverse=not self.reverse
+            self.step=0
+
+
---
+++ Fillers/Strobe.py
@@ -0,0 +1,39 @@
+
+from Fillers.Default import Default
+from RGB import RGB
+
+
+class Strobe(Default):
+    data_type = "Strobe"
+
+    def __init__(self, rate):
+        """
+        Init for steady color
+        :param args: for App
+        :param trail_length: length of snow trail
+        """
+        super().__init__(rate)
+
+        self.color = RGB(random=True)
+        self.alpha=255
+        self.delay=10
+        self.step=0
+
+
+    def fill(self):
+
+
+        if self.color.c>0 and self.step%self.delay!=0:
+            self.color.update_single(c=0)
+            self.step=0
+        if self.color.c == 0 and self.step % self.delay != 0:
+            self.color.update_single(c=self.alpha)
+            self.step=0
+
+        self.step+=1
+
+
+        for idx in range(self.strip_length):
+            self.color_set(idx,self.color)
+
+
