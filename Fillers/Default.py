b2c328ac2daefbbc29820c81cbf5ce4fff978a1a
dizzi <nicofirst1@gmail.com>
2019-11-22 15:22:25 +0100

    updates
    - drop args, using rate instead
    - add update params method

--- Fillers/Default.py
+++ Fillers/Default.py
@@ -5,17 +5,17 @@ from DotStar_Emulator.emulator.send_test_data import App
 
 from RGB import RGB
 from utils import bound_sub, circular_step
-
+import inspect
 
 class Default(App):
     data_type = ""
 
-    def __init__(self, args):
+    def __init__(self, rate):
         """
         Init for snow effect
         :param args:
         """
-        super().__init__(args)
+        super().__init__(rate)
 
         self.strip_length = self.grid_size.x + self.grid_size.y - 1
 
@@ -32,3 +32,10 @@ class Default(App):
     def on_loop(self):
         self.fill()
         self.send()
+
+    def update_args(self, **kwargs):
+        variables = [i for i in dir(self) if not inspect.ismethod(i)]
+
+        for k in kwargs.keys():
+            if k in variables:
+                setattr(self, k, 21)
