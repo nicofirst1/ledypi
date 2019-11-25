f3242477f35d3ef17db5477f1bbb63bb4ddb553f
dizzi <nicofirst1@gmail.com>
2019-11-25 21:35:19 +0100

    add import and patterns

---
+++ Fillers/__init__.py
@@ -0,0 +1,24 @@
+from Fillers.ColorWipe import ColorWipe
+from Fillers.Fading import Fading
+from Fillers.Fire import Fire
+from Fillers.FireWork import FireWork
+from Fillers.Meteor import Meteor
+from Fillers.Rainbow import Rainbow
+from Fillers.Snow import Snow
+from Fillers.Steady import Steady
+from Fillers.Strobe import Strobe
+
+
+
+Patterns=dict(
+    ColorWipe=ColorWipe,
+    Fading=Fading,
+    Fire=Fire,
+    FireWork=FireWork,
+    Meteor=Meteor,
+    Rainbow=Rainbow,
+    Snow=Snow,
+    Steady=Steady,
+    Strobe=Strobe,
+
+)
