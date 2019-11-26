3739f6cddab295c820b3d8116f3ec74d051d1c54
dizzi <nicofirst1@gmail.com>
2019-11-26 20:06:08 +0100

    fix

--- Fillers/FireWork.py
+++ Fillers/FireWork.py
@@ -1,8 +1,6 @@
 from copy import deepcopy
 from random import randint
 
-from DotStar_Emulator.emulator.send_test_data import App
-
 from Fillers.Default import Default
 from RGB import RGB
 from utils import bound_sub, circular_step
@@ -11,26 +9,23 @@ from utils import bound_sub, circular_step
 class FireWork(Default):
     data_type = "FireWork"
 
-    def __init__(self, args, num_of_fires=5, usa_add=False):
+    def __init__(self, delay, num_of_fires=5, usa_add=False):
         """
         Init for snow effect
-        :param args:
+        :param delay:
         """
-        super().__init__(args)
+        super().__init__(delay)
 
         self.fires = num_of_fires
-        self.use_add=usa_add
+        self.use_add = usa_add
         self.loss = 25
         self.strip_length = self.grid_size.x + self.grid_size.y - 1
         self.step = 0
-        self.centers = {randint(0, self.strip_length-1): self.empty_center() for _ in range(num_of_fires)}
+        self.centers = {randint(0, self.strip_length - 1): self.empty_center() for _ in range(num_of_fires)}
 
     def empty_center(self):
         return dict(color=RGB(random=True), tail=[], step=0)
 
-    def set(self, index, rgb, **kwargs):
-
-        super().set(index, rgb.c, rgb.r, rgb.b, rgb.b)
 
     def fill(self):
 
@@ -42,7 +37,7 @@ class FireWork(Default):
 
             # get the color and the tail
             color = attr["color"]
-            step=attr["step"]
+            step = attr["step"]
             has_popped = False
 
             # estimate the center intesity and update
@@ -73,14 +68,14 @@ class FireWork(Default):
                     # update tail
                     attr["tail"].append((p, n, idx))
                 # remove duplicates
-                attr["tail"]= list(set( attr["tail"]))
+                attr["tail"] = list(set(attr["tail"]))
 
             # if the center has faded then the tails need to fade too
             else:
                 # if there are some non zero tail
-                if len( attr["tail"]) > 0:
+                if len(attr["tail"]) > 0:
                     # for every tail
-                    for t in  attr["tail"]:
+                    for t in attr["tail"]:
                         # get previous, next and index
                         p = t[0]
                         n = t[1]
@@ -89,55 +84,46 @@ class FireWork(Default):
                         ci = bound_sub(255, self.loss * (loss_weight * step + 1 - idx))
                         # update
                         color.update_single(c=ci)
-                        self.add_update_pixel(p,color)
-                        self.add_update_pixel(n,color)
+                        self.add_update_pixel(p, color)
+                        self.add_update_pixel(n, color)
                         # if ci is zero remove point from tail
                         if ci == 0:
-                            attr["tail"].pop( attr["tail"].index(t))
+                            attr["tail"].pop(attr["tail"].index(t))
 
                 # if the center is faded and it has no more tail
                 else:
                     # remove the center
                     self.centers.pop(c)
                     # get another one which is not in the center lists already
-                    rd = randint(0, self.strip_length-1)
+                    rd = randint(0, self.strip_length - 1)
                     while rd in self.centers.keys():
-                        rd = randint(0, self.strip_length-1)
+                        rd = randint(0, self.strip_length - 1)
                     # put random color
                     self.centers[rd] = self.empty_center()
-                    has_popped=True
+                    has_popped = True
 
             # is the center has been removed then dont update
             if not has_popped:
                 self.centers[c]["tail"] = attr["tail"]
-                step=circular_step(step,self.strip_length)
-                self.centers[c]["step"]=step
+                step = circular_step(step, self.strip_length)
+                self.centers[c]["step"] = step
 
         self.update_couter()
 
-
-
-        self.set_pixels()
-
     def add_update_pixel(self, idx, new_color):
 
-        current_color=self.pixels[idx]['color']
+        self.pixels[idx]['color'] = new_color
+        # current_color = self.pixels[idx]['color']
 
-
-
-        if current_color.same_color(new_color) or current_color.is_gray()  :
-            self.pixels[idx]['color'].update_color(new_color)
-        elif self.use_add:
-            self.pixels[idx]['color'].add_colors(new_color)
-        else:
-            self.pixels[idx]['color'].update_color(new_color)
-
-
+        # if current_color.same_color(new_color) or current_color.is_gray():
+        #     self.pixels[idx]['color']=new_color
+        # elif self.use_add:
+        #     self.pixels[idx]['color'].add_colors(new_color)
+        # else:
+        #     self.pixels[idx]['color']=new_color
 
     def update_couter(self):
         self.step += 1
         self.step %= self.strip_length
 
-    def on_loop(self):
-        self.fill()
-        self.send()
+
