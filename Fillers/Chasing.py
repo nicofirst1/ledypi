fe36cd7eee1fc546caeaa5be10f08216f99cbae9
dizzi <nicofirst1@gmail.com>
2019-11-28 19:44:23 +0100

    add randomize

--- Fillers/Chasing.py
+++ Fillers/Chasing.py
@@ -11,35 +11,33 @@ class Chasing(Default):
     def __init__(self, **kwargs):
 
         super().__init__(**kwargs)
-        self.color = RGB(random=True)
-        self.size=1
-        self.trail_decay=64
-        self.random_decay=True
-        self.step=0
-        self.increasing=True
-
+        self.size = 1
+        self.trail_decay = 64
+        self.random_decay = True
+        self.step = 0
+        self.increasing = True
+        self.second_color = RGB(random=True)
 
     def fill(self):
 
-
-
         for jdx in range(self.strip_length):
-            if not self.random_decay or randint(0,10) > 5:
+            if not self.random_decay or randint(0, 10) > 5:
                 self.pixels[jdx]['color'].fade(self.trail_decay)
 
-        for jdx in range( 0, self.size):
-            if self.step-jdx< self.strip_length and self.step-jdx>=0:
-                self.pixels[self.step-jdx]['color']=self.color.copy()
+        for jdx in range(0, self.size):
+            if self.step - jdx < self.strip_length and self.step - jdx >= 0:
+                self.pixels[self.step - jdx]['color'] = self.second_color.copy()
 
         if 0 <= self.step < self.strip_length and self.increasing:
             self.step = bound_add(self.step, 1, maximum=255)
         elif 0 < self.step <= self.strip_length and not self.increasing:
             self.step = bound_sub(self.step, 1, minimum=0)
         else:
-            self.increasing=not self.increasing
-            self.color=RGB(random=True)
+            self.increasing = not self.increasing
 
+            if self.randomize_color:
 
+                self.second_color = RGB(random=True)
 
-
-
+            else:
+                self.second_color = self.color
