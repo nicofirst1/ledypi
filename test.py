0d1ff52a766d956c3d9b497372f7a170df32a821
dizzi <nicofirst1@gmail.com>
2020-06-23 10:25:14 +0200

    rename

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from Patterns import Patterns, ColorWipe
 
 from RGB import RGB
 
-pat= Patterns['Fading']
+pat= Patterns['Chasing']
 rate=10
 color=RGB(white=True)
 config=dict(rate=rate, color=color)
-app= pat(config)
+app= pat(rate=rate, color=color)
 app.run()
