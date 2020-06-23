dc14f15dc81b45225d7c08ce6888036c0c686509
dizzi <nicofirst1@gmail.com>
2020-06-23 11:55:51 +0200

    fixed argparse

--- src/firebase/connect.py
+++ src/firebase/connect.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse private key json file.')
     parser.add_argument('credential', type=str,
                         help='The path to the private key json file for Firebase')
-    parser.add_argument('--databaseURL', type=str,
+    parser.add_argument('--databaseURL', type=str, nargs='?',
                         const=sum, default="https://ledypie.firebaseio.com/",
                         help='The Firebase database url')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
-    fbc = FireBaseConnector(credential_path=args['credential'], database_url=args['databaseURL'])
+    print(args)
+    fbc = FireBaseConnector(credential_path=args.credential, database_url=args.databaseURL)
