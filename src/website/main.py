5d96403972c36a96fc36d15b9375917962a4e0ec
dizzi <nicofirst1@gmail.com>
2020-06-24 15:01:22 +0200

    add website

---
+++ src/website/main.py
@@ -0,0 +1,20 @@
+import network
+
+def connect():
+    ssid = "yourNetworkName"
+    password = "yourNetworkPassword"
+
+    station = network.WLAN(network.STA_IF)
+
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+
+    station.active(True)
+    station.connect(ssid, password)
+
+    while station.isconnected() == False:
+        pass
+
+    print("Connection successful")
+    print(station.ifconfig())
