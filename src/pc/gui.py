357e2a596d6c9b75523ecebbf2df856dc27aeaf4
dizzi <nicofirst1@gmail.com>
2020-07-23 15:30:10 +0200

    add python path

--- src/pc/gui.py
+++ src/pc/gui.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+root = dir_path.split("ledypi")[0] + "ledypi"
+
+paths = [f"{root}/DotStar_Emulator", f"{root}/src"]
+sys.path += paths
+
+
 from DotStar_Emulator.emulator import EmulatorApp
 
 app = EmulatorApp()
