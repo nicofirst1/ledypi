3a28ea8285a64681e226de6204f2370d70e7a564
dizzi <nicofirst1@gmail.com>
2020-07-23 12:43:43 +0200

    ignore resourced dir

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,7 +3,7 @@
 pi_ip=192.168.178.53 # the ip of your rpi
 repo_location=""     # Where to sync in the rpi
 
-files=(.git __pycache__ .idea audio-reactive-led-strip/images )
+files=(.git __pycache__ .idea audio-reactive-led-strip/images Resources )
 excludes=()
 for f in "${files[@]}"; do
   excludes+=(--exclude "$f")
