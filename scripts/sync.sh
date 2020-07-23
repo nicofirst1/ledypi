ffc343f09f4fd38f7989d7a448f4ead6143bde21
dizzi <nicofirst1@gmail.com>
2020-07-23 10:45:34 +0200

    split installation instruction from readme

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,7 +3,7 @@
 pi_ip=192.168.178.53 # the ip of your rpi
 repo_location=""     # Where to sync in the rpi
 
-files=(.git __pycache__ .idea)
+files=(.git __pycache__ .idea audio-reactive-led-strip/images )
 excludes=()
 for f in "${files[@]}"; do
   excludes+=(--exclude "$f")
