2a5915ff883e078ba5dd4088dd3fbf84a32c6320
dizzi <nicofirst1@gmail.com>
2020-07-27 14:02:09 +0200

    replaced perlin with opensimplex

--- src/patterns/water.py
+++ src/patterns/water.py
@@ -1,9 +1,10 @@
-import numpy as np
+import sys
+
+from opensimplex import OpenSimplex
 
 from patterns.default import Default
 from rgb import RGB
-from utils.color import bound_add
-from utils.perlin_noise import generate_perlin_noise_2d
+from utils.color import bound_add, scale
 
 
 class Water(Default):
@@ -12,20 +13,14 @@ class Water(Default):
 
         super().__init__(**kwargs)
 
-        self.increasing = False
         self.pattern_name = "Water"
 
         self.x_div = 2 ** 4  # horizontal divisor
-        self.y_div = self.strip_length // 15  # vertical divisor
-        self.deepness = 20  # measure of blueness (the less the more blue)
+        self.y_div = 2 ** 4  # vertical divisor
+        self.deepness = 100  # measure of blueness (the more the more blue)
 
-        self.max = 2 ** 10  ## height of the noise image, will be diplayed on the time axis
-        self.base_b = 20  # min value for the blue
-
-        # generate and normalize noise in range 0-255
-        noise = generate_perlin_noise_2d((self.max, self.strip_length), (self.x_div, self.y_div))
-        self.noise = (noise + 1) * 255 / 2
-        self.noise = self.noise.astype(np.int)
+        self.op = OpenSimplex()
+        self.counter = 0
 
         self.counter = 0
 
@@ -41,22 +36,21 @@ class Water(Default):
 
     @y_div.setter
     def y_div(self, value):
-        self._y_div = self.strip_length // value
+        self._y_div = value
 
     def fill(self):
 
         # reset counter
-        if self.counter == self.max:
+        if self.counter == sys.maxsize - 1:
             self.counter = 0
-
-        # get a slice from the noise image
-        slice = self.noise[self.counter, :]
 
         # color
         for idx in range(self.strip_length):
-            val = slice[idx] \
-                # add base value + deepness
-            val = bound_add(val, 255 // self.deepness + self.base_b)
+            val = self.op.noise2d(idx / self.x_div, self.counter / self.y_div)
+            val = scale(val, 0, 255, -1, 1)
+
+            # add base value + deepness
+            val = bound_add(val, self.deepness)
             self.pixels[idx]['color'] = RGB(b=val, g=255 - val, a=self.color.a)
 
         # update counter
