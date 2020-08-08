6bcb3b58c0b38e2906c89325aa48691eafaa453f
dizzi <nicofirst1@gmail.com>
2020-08-08 12:32:52 +0200

    fixed rgb handler

--- src/rpi/pi_handler.py
+++ src/rpi/pi_handler.py
@@ -9,7 +9,7 @@ class PiHandler(object):
         import board
 
         # init the pixel strip
-        self.np = neopixel.NeoPixel(board.D18, pixels, auto_write=False, pixel_order=neopixel.RGB)
+        self.np = neopixel.NeoPixel(board.D18, pixels, auto_write=False)
         self.pixel_count = pixels
 
     def set(self, index, a, b, g, r):
