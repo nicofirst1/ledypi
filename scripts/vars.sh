1ec7e59f6dcd9350178653d61315c68834d2263f
dizzi <nicofirst1@gmail.com>
2020-09-12 15:25:27 +0200

    fixed absolute path

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,6 +1,7 @@
-path2repo="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd))"
-
+path2repo="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+path2repo=${path2repo%/ledypi*}
+path2repo=$path2repo/ledypi
 echo "Path to ledypi is : $path2repo"
 
-pythonpath="$path2repo/src:$path2repo/audio-reactive-led-strip/src:$path2repo/ledyweb"
+pythonpath="$path2repo:$path2repo/src:$path2repo/audio-reactive-led-strip/src:$path2repo/ledyweb"
 export PYTHONPATH="$pythonpath"
